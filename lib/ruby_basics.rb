7474bd45473e2fe0041eac8eb869e8a8eb64d8aa
Cheng Vue <cvu3@ucsc.edu>
2018-11-09 18:53:14 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,16 +1,16 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  var = value
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
